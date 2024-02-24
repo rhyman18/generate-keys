8820dbf4310b5e28eb885a1f3bd6347ab090de49
Ari Budiman <rhyman18@gmail.com>
2024-02-25 04:03:25 +0700

    feat: initial commit version 1.0.0

---
+++ App.js
@@ -0,0 +1,15 @@
+import inquirer from "inquirer";
+import { printRandomString } from "./script/PrintRandomString.js";
+
+inquirer
+  .prompt([
+    {
+      type: "list",
+      name: "length",
+      message: "Mau bikin keys yang mana cuy?",
+      choices: [new inquirer.Separator(), "16 digit xkey", "40 digit xkey"],
+    },
+  ])
+  .then((answers) => {
+    printRandomString(answers);
+  });
---
+++ script/PrintRandomString.js
@@ -0,0 +1,32 @@
+import { generateRandomString } from "./GenerateRandomString.js";
+
+const styles = {
+  reset: "\x1b[0m",
+  bold: "\x1b[1m",
+  fgGreen: "\x1b[32m",
+  fgCyan: "\x1b[36m",
+  fgRed: "\x1b[31m",
+};
+
+const greaterSign = `${styles.fgGreen}>${styles.reset}`;
+
+const printRandomString = (answers) => {
+  const selectedLength = parseInt(answers.length.match(/\d+/)[0]);
+
+  console.log("\r");
+
+  if (!isNaN(selectedLength)) {
+    console.log(`${greaterSign} Proses generate...`);
+    const randomString = generateRandomString(selectedLength);
+    console.log(`${greaterSign} Berhasil generate, gass copas mang!\n`);
+    console.log(
+      styles.bold + styles.fgCyan + randomString + styles.reset + "\n"
+    );
+  } else {
+    console.log(
+      `${greaterSign} ${styles.fgRed}Loh kok bisa pilih yang lain?${styles.reset}`
+    );
+  }
+};
+
+export { printRandomString };
