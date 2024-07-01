6d9842b15929e85b0b77da44c661df025063635c
rhyman18 <rhyman18@gmail.com>
2024-07-01 23:12:56 +0700

    fix: module path, update dependencies

--- dist/index.d.cts
+++
@@ -1,3 +0,0 @@
-declare const generateKeys: (length?: number) => string;
-
-export { generateKeys };
--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,29 @@
+"use strict";
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// src/index.ts
+var src_exports = {};
+__export(src_exports, {
+  generateKeys: () => generateKeys
+});
+module.exports = __toCommonJS(src_exports);
+
 // src/script/GenerateRandomString.ts
 var generateRandomString = (length = 40) => {
   const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -19,6 +45,7 @@ var generateKeys = (length = 40) => {
   }
   return GenerateRandomString_default(length);
 };
-export {
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
   generateKeys
-};
+});
